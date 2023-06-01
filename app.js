a05fcfbd39d5ad19b01eda7c2f1203bddce22ee9
Jean-Sébastien Dupuis <js.dupuis@free.fr>
2023-06-01 14:26:05 +0200

    ajout env name

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+
+const env = process.env.ENV_NAME;
 app.get("/", function (req, res) {
-  res.send(`Hello World!`)
-})
+    res.send(`Hello World! Evironnement de  ${env}`);
+});
 
 const port = process.env.PORT
 app.listen(port, function () {
